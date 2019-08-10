9d6541517d4be4f3e7073030f4f2c043217320d3
Helena Paixão <hp.helenapaixao@gmail.com>
2019-08-10 15:34:06 -0300

    api finalizada, verificar o index

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const server = express();
+mongoose.connect('mongodb+srv://app01:app01@cluster0-d1swx.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+
+server.use(cors())
+server.use(express.json())
+server.use(routes)
+
+
+server.listen(3333)
